24ea671a989a5a4471f5ade59201611f16b94095
fpem3309 <alsrhks2154@gmail.com>
2022-02-23 11:45:25 +0900

    navheader_set

--- app/src/main/java/com/example/companymarket/MainActivity.kt
+++ app/src/main/java/com/example/companymarket/MainActivity.kt
@@ -3,6 +3,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
+import android.widget.TextView
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -12,12 +13,13 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.example.companymarket.databinding.ActivityMainBinding
+import com.example.companymarket.databinding.NavHeaderMainBinding
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
-    val user = Firebase.auth.currentUser
+    private val user = Firebase.auth.currentUser
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
@@ -27,8 +29,9 @@ class MainActivity : AppCompatActivity() {
         var uid = user?.uid
         Log.d("userdata", uid.toString())// user uid가져오기
 
+
      binding = ActivityMainBinding.inflate(layoutInflater)
-     setContentView(binding.root)
+        setContentView(binding.root)
 
         setSupportActionBar(binding.appBarMain.toolbar)
 
@@ -43,13 +46,16 @@ class MainActivity : AppCompatActivity() {
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
 
+        var header =  navView.getHeaderView(0)
+        var tv_userEmail : TextView = header.findViewById(R.id.tv_userEmail)
+        tv_userEmail.text = uid.toString()
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(setOf(
             R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-
 //        @Database(entities = [User::class], version = 1)
 //        abstract class AppDatabase : RoomDatabase() {
 //            abstract fun userDao(): UserDao
--- app/src/main/java/com/example/companymarket/NewProductActivity.java
+++ app/src/main/java/com/example/companymarket/NewProductActivity.java
@@ -1,23 +1,28 @@
 package com.example.companymarket;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class NewProductActivity extends AppCompatActivity {
     EditText edt_productName, edt_productPrice, edt_productStatus, edt_productContent;
-    Button btn_addProduct;
-
+    Button btn_addProduct, btn_addImage;
+    ImageView iv_preview;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,16 +33,30 @@ public class NewProductActivity extends AppCompatActivity {
         edt_productStatus = findViewById(R.id.edt_productStatus);
         edt_productContent = findViewById(R.id.edt_productContent);
         btn_addProduct = findViewById(R.id.btn_addProduct);
+        btn_addImage = findViewById(R.id.btn_addImage);
+        iv_preview = findViewById(R.id.iv_preview);
+
+        btn_addImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseStorage storage = FirebaseStorage.getInstance(); //스토리지 인스턴스 만들기
+                StorageReference storageReference = storage.getReference(); //스토리지 참조
+
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+                startActivityForResult(intent,10);
+            }
+        });
+
 
         btn_addProduct.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 FirebaseDatabase database = FirebaseDatabase.getInstance(); // 파이어 데이터베이스 연동
-
                 DatabaseReference databaseReference = database.getReference(); // 파이어베이스 테이블 연결
 
-                databaseReference.child("Product").push().setValue(new Product("gs://companymarket-122c7.appspot.com/사진.png",edt_productName.getText().toString(),Integer.parseInt(edt_productPrice.getText().toString()),edt_productStatus.getText().toString(),edt_productContent.getText().toString()));
+                databaseReference.child("Product").push().setValue(new Product("https://firebasestorage.googleapis.com/v0/b/companymarket-122c7.appspot.com/o/%EC%82%AC%EC%A7%84.png?alt=media&token=1b3cff9d-33bd-4042-870d-e191c55515f7",edt_productName.getText().toString(),Integer.parseInt(edt_productPrice.getText().toString()),edt_productStatus.getText().toString(),edt_productContent.getText().toString()));
                 //Map<String,Product> productMap = new HashMap<>();
                 //productMap.put("test",new Product("",edt_productName.getText().toString(),Integer.parseInt(edt_productPrice.getText().toString()),edt_productStatus.getText().toString(),edt_productContent.getText().toString()));
 
